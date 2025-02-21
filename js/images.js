4064d6fa76efbfff84195f7b0cce5f5d39ea4b2c
Rocky <rockwoodfrank@gmail.com>
2025-02-20 21:51:20 -0800

    Add some images and a script

---
+++ js/images.js
@@ -0,0 +1,19 @@
+var folder = "test/";
+
+fetch(folder)
+    .then(response => response.text())  // Get response as text (HTML directory listing)
+    .then(data => {
+        let parser = new DOMParser();
+        let doc = parser.parseFromString(data, "text/html"); // Parse as HTML
+
+        doc.querySelectorAll("a").forEach(link => {
+            let href = link.getAttribute("href");
+
+            if (href && href.match(/\.(jpe?g|png|gif)$/i)) { // Check for image file extensions
+                let img = document.createElement("img");
+                img.src = folder + href;
+                document.body.appendChild(img);
+            }
+        });
+    })
+    .catch(error => console.error("Error fetching folder:", error));
