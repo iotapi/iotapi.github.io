63665d0b8ab4023071227b77e3fcbfdc1558782b
Rocky <rockwoodfrank@gmail.com>
2025-02-21 16:50:42 -0800

    Add lazy loading and a placeholder image

--- js/images.js
+++ js/images.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
     .then(response => response.json())
     .then(files => {
         const gallery = document.getElementById("gallery");
+        let imagePromises = [];
 
         // Sort images by upload date (newest first)
         files.sort((a, b) => new Date(b.uploadDate) - new Date(a.uploadDate));
@@ -15,11 +16,25 @@ document.addEventListener("DOMContentLoaded", function () {
         // Display sorted images
         files.forEach(file => {
             const imgElement = document.createElement("img");
-            imgElement.src = `pictures/${file.filename}`;
+            imgElement.className = "lazy-image"
+            imgElement.src = "img/placeholder.jpg"
+            imgElement.dataset.src = `pictures/${file.filename}`;
             imgElement.alt = file.filename;
             gallery.appendChild(imgElement);
+
+            let imgPromise = new Promise(resolve => {
+                imgElement.onload = resolve;
+                imgElement.onerror = resolve; // Resolve even if image fails to load
+            });
+            imagePromises.push(imgPromise);
         });
+
+        return Promise.all(imagePromises);
+    })
+    .then(() => {
+        lazyLoadImages('.lazy-image', { threshold: 0.7 });
     })
     .catch(error => console.error("Error loading images:", error));
+
 
 });
---
+++ js/imports.js
@@ -0,0 +1,1 @@
+import { lazyLoadImages } from './lazyLoad.js';
