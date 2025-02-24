80a65df07ee219c8a2268e1b2dcf384171f82463
Rocky <rockwoodfrank@gmail.com>
2025-02-24 10:49:43 -0800

    Fix a bug where the images weren't sorting

--- js/pictures.js
+++ js/pictures.js
@@ -11,10 +11,11 @@ document.addEventListener("DOMContentLoaded", function () {
         let imagePromises = [];
 
         // Sort images by upload date (newest first)
-        files.sort((a, b) => new Date(b.uploadDate) - new Date(a.uploadDate));
+        files.sort((a, b) => new Date(b.upload_date) - new Date(a.upload_date));
 
         // Display sorted images
         files.forEach(file => {
+            console.log(file.upload_date);
             const imgElement = document.createElement("img");
             imgElement.className = "lazy-image"
             imgElement.src = "img/placeholder.jpg"
