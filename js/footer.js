4c7d44544c794c50fbdbd111a31af1421a2f5650
Rocky <rockwoodfrank@gmail.com>
2025-09-20 19:24:25 -0700

    Update Footer

--- js/footer.js
+++ js/footer.js
@@ -3,7 +3,13 @@
 const footer__template = document.createElement('template');
 
 footer__template.innerHTML = `
-    <p>Hello :)</p>
+    <div>
+    <p>Box 444, University Union Epicenter<br />
+    California Polytechnic State University<br />
+    San Luis Obispo, CA 93407-0675 <br />
+    </p>
+    </div>
+    <div>©${new Date().getFullYear()} Kappa Kappa Psi - Iota Pi. All Rights Reserved</div>
 `;
 
 const footer__element = document.getElementById('footer');
